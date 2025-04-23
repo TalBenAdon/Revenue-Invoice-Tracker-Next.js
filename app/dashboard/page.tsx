cbe48238abbf822667152f5f6dd538968aab92b0
Tal Benadon <talbenadon2@gmail.com>
2025-04-23 16:48:28 +0900

    structured dashboard/page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,23 @@
-export default function page() {
+import { lusitana } from "../ui/fonts";
+
+export default async function page() {
     return (
-        <p>Dashboard page</p>
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+                </h1>
+                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <div>Card</div>
+                <div>Card</div>
+                <div>Card</div>
+                <div>Card</div>
+                </div>
+                <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                    Chart
+                    chart
+                    Chart
+                    chart
+                </div>
+        </main>
     )
 }
