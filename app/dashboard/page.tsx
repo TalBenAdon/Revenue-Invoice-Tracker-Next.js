87c5d8a0f4a66aea2d33722b915884b694c40776
Tal Benadon <talbenadon2@gmail.com>
2025-04-23 17:18:31 +0900

    enabled representational revenue chart component, added to dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { lusitana } from "../ui/fonts";
-
+import { fetchRevenue } from "../lib/data";
+import RevenueChart from "../ui/dashboard/revenue-chart";
 export default async function page() {
+    const revenue = await fetchRevenue();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -13,7 +15,7 @@ export default async function page() {
                 <div>Card</div>
                 </div>
                 <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                    Chart
+                    <RevenueChart revenue={revenue}/>
                     chart
                     Chart
                     chart
